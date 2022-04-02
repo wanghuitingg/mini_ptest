991c90e8819aeaf95b68002594411022a500f8d8
wanghuitingg <1687752378@qq.com>
2022-04-02 23:58:38 +0800

    402

--- miniprogram/pages/cloud_fun/cloud_fun.js
+++ miniprogram/pages/cloud_fun/cloud_fun.js
@@ -1,6 +1,7 @@
 // pages/cloud_fun/cloud_fun.js
 const db = wx.cloud.database();
 const dataDB = db.collection("test_db");
+const _ = db.command;
 Page({
 
   /**
@@ -17,6 +18,11 @@ Page({
         color:"blue",
         school:"北京舞蹈学院",
         location: new db.Geo.Point(113, 23),
+        age:22,
+        arr: [1,1,1],
+        parent:{
+          name:"扭扭"
+        }
       }
     }).then((res)=>{
       console.log(res);
@@ -30,9 +36,59 @@ Page({
   },
   // 点击条件查询
   getByHandle(){
-    dataDB.where({
-      school:"北京舞蹈学院"
-    }).get().then((res)=>{
+    // dataDB.where({
+    //   school:"北京舞蹈学院"
+    // }).get().then((res)=>{
+    //   console.log(res);
+    // })
+    // dataDB.where({
+    //   // age大于21的
+    //   age:_.gt(21)
+    // }).get().then((res)=>{
+    //   console.log(res);
+    // })
+    // dataDB.where({
+    //   // 数组包含4的
+    //   arr:_.in([4])
+    // }).get().then((res)=>{
+    //   console.log(res);
+    // })
+    // dataDB.where({
+    //   "parent.name":"扭扭"
+    // }).get().then((res)=>{
+    //   console.log(res);
+    // })
+    // dataDB.limit(5).skip(8).get().then((res)=>{
+    //   console.log(res);
+    // })
+    dataDB.count().then((res)=>{
+      console.log(res);
+    })
+  },
+  // 点击更新
+  editHandle(){
+    // dataDB.doc("41ae62ef622f3e9f0c57ff900ec1a516").update({
+    //   data:{
+    //     年龄+2
+    //     age:_.inc(2)
+    //   },  
+    //   success:function (res) {
+    //     console.log(res);
+    //   }
+    // // }).get().then((res)=>{
+    // //   console.log(res);
+    // })
+    dataDB.doc("41ae62ef622f3e9f0c57ff900ec1a516").update({
+      data:{
+        name:"刘宇啾咪"
+      }
+    }).then((res)=>{
+      console.log(res);
+    })
+  },
+  // 点击删除
+  deleteHandle(){
+    dataDB.doc("41ae62ef622f436d0c58c3bd6e9399a1").remove().then((res)=>{
       console.log(res);
     })
   },
